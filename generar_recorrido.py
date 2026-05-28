c371b0a2867e2390323440f959d1591017c428fe
vvenceslavv <wenceslao.arroyo@netvalle.com.mx>
2026-05-27 23:28:38 -0600

    UI-Feature: Street View real de alta densidad con 50 nodos y mapa sincronizado

---
+++ generar_recorrido.py
@@ -0,0 +1,108 @@
+{
+  "default": {
+    "firstScene": "nodo_frame_00000",
+    "author": "NetValle",
+    "autoLoad": true,
+    "compass": true
+  },
+  "scenes": {
+    "nodo_frame_00000": {
+      "title": "Progreso del Corredor - 0m (frame_00000)",
+      "type": "equirectangular",
+      "panorama": "frame_00000.jpg",
+      "hotSpots": [
+        {
+          "pitch": -8,
+          "yaw": 0,
+          "type": "scene",
+          "text": "Avanzar",
+          "sceneId": "nodo_frame_00001"
+        }
+      ]
+    },
+    "nodo_frame_00001": {
+      "title": "Progreso del Corredor - 4m (frame_00001)",
+      "type": "equirectangular",
+      "panorama": "frame_00001.jpg",
+      "hotSpots": [
+        {
+          "pitch": -8,
+          "yaw": 0,
+          "type": "scene",
+          "text": "Avanzar",
+          "sceneId": "nodo_frame_00002"
+        },
+        {
+          "pitch": -8,
+          "yaw": 180,
+          "type": "scene",
+          "text": "Regresar",
+          "sceneId": "nodo_frame_00000"
+        }
+      ]
+    },
+    "nodo_frame_00002": {
+      "title": "Progreso del Corredor - 8m (frame_00002)",
+      "type": "equirectangular",
+      "panorama": "frame_00002.jpg",
+      "hotSpots": [
+        {
+          "pitch": -8,
+          "yaw": 0,
+          "type": "scene",
+          "text": "Avanzar",
+          "sceneId": "nodo_frame_00003"
+        },
+        {
+          "pitch": -8,
+          "yaw": 180,
+          "type": "scene",
+          "text": "Regresar",
+          "sceneId": "nodo_frame_00001"
+        }
+      ]
+    },
+    "nodo_frame_00003": {
+      "title": "Progreso del Corredor - 12m (frame_00003)",
+      "type": "equirectangular",
+      "panorama": "frame_00003.jpg",
+      "hotSpots": [
+        {
+          "pitch": -8,
+          "yaw": 0,
+          "type": "scene",
+          "text": "Avanzar",
+          "sceneId": "nodo_frame_00004"
+        },
+        {
+          "pitch": -8,
+          "yaw": 180,
+          "type": "scene",
+          "text": "Regresar",
+          "sceneId": "nodo_frame_00002"
+        }
+      ]
+    },
+    "nodo_frame_00004": {
+      "title": "Progreso del Corredor - 16m (frame_00004)",
+      "type": "equirectangular",
+      "panorama": "frame_00004.jpg",
+      "hotSpots": [
+        {
+          "pitch": -8,
+          "yaw": 0,
+          "type": "scene",
+          "text": "Avanzar",
+          "sceneId": "nodo_frame_00005"
+        },
+        {
+          "pitch": -8,
+          "yaw": 180,
+          "type": "scene",
+          "text": "Regresar",
+          "sceneId": "nodo_frame_00003"
+        }
+      ]
+    },
+    "nodo_frame_00005": {
+      "title": "Progreso del
